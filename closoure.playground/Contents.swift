868f02e13992f6e1b1b72aca1ddbec807fbe108b
Asraful Alam <asraful.alam@mobioapp.com>
2020-04-22 13:49:21 +0600

    update example

--- closoure.playground/Contents.swift
+++ closoure.playground/Contents.swift
@@ -1,9 +1,9 @@
 import UIKit
 
 let numbers = [1,2,3,4,5,6,7,8,9,10]
-let filteredList = filterWithPredicateClosure(closure:{(num) -> Bool in
+/*let filteredList = filterWithPredicateClosure(closure:{(num) -> Bool in
     return num > 5
-},numbers: numbers)
+},numbers: numbers)*/
 
 
 func filterWithPredicateClosure(closure:(Int) -> Bool,numbers:[Int]) -> [Int] {
@@ -16,4 +16,14 @@ func filterWithPredicateClosure(closure:(Int) -> Bool,numbers:[Int]) -> [Int] {
     return findingNums
 }
 
+func greaterThenThree(value: Int) -> Bool{
+    return value > 3
+}
+
+func dividedByFive(value:Int) -> Bool{
+    return value % 5 == 0
+}
+
+//let filteredList = filterWithPredicateClosure(closure: greaterThenThree, numbers: numbers)
+let filteredList = filterWithPredicateClosure(closure: dividedByFive, numbers: numbers)
 print(filteredList)
