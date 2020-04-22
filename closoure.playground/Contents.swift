c633d2fff49558dbcff3fe20734f55bb3af93272
Asraful Alam <asraful.alam@mobioapp.com>
2020-04-23 01:06:26 +0600

    Difference btw nonescaping and escaping- using closure

--- closoure.playground/Contents.swift
+++ closoure.playground/Contents.swift
@@ -2,8 +2,8 @@ import UIKit
 
 let numbers = [1,2,3,4,5,6,7,8,9,10]
 /*let filteredList = filterWithPredicateClosure(closure:{(num) -> Bool in
-    return num > 5
-},numbers: numbers)*/
+ return num > 5
+ },numbers: numbers)*/
 
 
 func filterWithPredicateClosure(closure:(Int) -> Bool,numbers:[Int]) -> [Int] {
@@ -27,3 +27,39 @@ func dividedByFive(value:Int) -> Bool{
 //let filteredList = filterWithPredicateClosure(closure: greaterThenThree, numbers: numbers)
 let filteredList = filterWithPredicateClosure(closure: dividedByFive, numbers: numbers)
 print(filteredList)
+
+// - Difference btw nonescaping and escaping-
+
+// nonescaping.....
+/*func addingTwoNumber(completion:(Int,Int) -> Int,num_a: Int, num_b: Int) -> Int{
+    print("start call completion")
+    let sum = completion(num_a,num_b)
+    print("end call completion")
+    return sum
+}
+
+let sum = addingTwoNumber(completion: { (a, b) -> Int in
+    return a + b
+}, num_a: 5, num_b: 10)
+
+print(sum)*/
+
+// escaping.....
+func addingTwoNumber(completion:@escaping(Int,Int) -> Int,num_a: Int, num_b: Int) -> Int{
+    print("start call completion")
+    
+    var sum = Int()
+    DispatchQueue.main.asyncAfter(deadline:  .now()+2) {
+        sum = completion(num_a,num_b)
+        print("end call completion:",sum)
+    }
+    print("end without completion and completion will end after deadline.")
+    return sum
+}
+
+let sum = addingTwoNumber(completion: { (a, b) -> Int in
+    return a + b
+}, num_a: 5, num_b: 10)
+
+print(sum)
+
